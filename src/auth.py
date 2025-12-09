c7611a7b329b634208e6ce4f4ee73a2af673b370
Alex-VanTassel <alexandre.vantassel@yale.edu>
2025-12-09 14:25:30 -0500

    Debugged authentication pipeline, access tokens now written to separate json file

--- src/auth.py
+++ src/auth.py
@@ -5,14 +5,24 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 import requests
 from base64 import b64encode
+import json
+import sys
+
+# ---------- Auxillary funciton to read json file ----------
+def load_info():
+    with open("info.json", "r") as f:
+        return json.load(f)
 
 # ---------- Local HTTP server to catch the auth code ----------
 class OAuthHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        '''
+        Handles get requests to the server
+        '''
         parsed_url = urlparse(self.path)
         qs = parse_qs(parsed_url.query)
         if "code" in qs:
-            self.server.auth_code = qs["code"][0]
+            self.server.auth_code = qs["code"][0] # Sets a server attribute to be the authentication code
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -20,9 +30,14 @@ class OAuthHandler(BaseHTTPRequestHandler):
 
 server = HTTPServer(("127.0.0.1", 8080), OAuthHandler)
 
+# ---------- Retrieving environment info from json ----------
+config = load_info()
+
+client_id = config['client_id']
+redirect_uri = config['redirect_uri']
+client_secret = config['client_secret']
+
 # ---------- Build Spotify auth URL ----------
-client_id = "582070c6a93749a1bc3826ac9ad8a32a"
-redirect_uri = "http://127.0.0.1:8080/callback"
 scopes = "user-library-read user-top-read playlist-read-private"
 
 auth_url = (
@@ -36,11 +51,11 @@ webbrowser.open(auth_url)
 # ---------- Wait for code ----------
 print("Waiting for authorization...")
 server.handle_request()
-code = server.auth_code
+code = getattr(server, 'auth_code', None)
 print("Received code:", code)
 
 # ---------- Exchange code for access + refresh tokens ----------
-client_secret = "SECRET_KEY"
+
 auth_str = f"{client_id}:{client_secret}"
 b64_auth_str = b64encode(auth_str.encode()).decode()
 
@@ -60,5 +75,10 @@ tokens = resp.json()
 access_token = tokens["access_token"]
 refresh_token = tokens["refresh_token"]
 
-print("Access token:", access_token)
-print("Refresh token:", refresh_token)
+# ---------- Write access and refresh tokens to json ----------
+
+config['access_token'] = access_token
+config['refresh_token'] = refresh_token
+
+with open('info.json', 'w') as f:
+    json.dump(config, f, indent=4)
